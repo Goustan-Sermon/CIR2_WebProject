d01f0d6ba266f5626b8a529640c5bcb5847a4160
Vincent Rocher <vincent.rocher@isen-ouest.yncrea.fr>
2023-04-04 14:39:47 +0200

    test php

---
+++ php/test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+ <head>
+   <link href="file.css" rel="stylesheet">
+   <meta charset="utf-8">
+   <title> index </title>
+ </head>
+ <body>
+   texte
+ </body>
+</html>
