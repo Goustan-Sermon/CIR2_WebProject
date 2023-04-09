77288f2f1cca4ad7421caf71fdd36cc83cba70ab
Vincent Rocher <vincent.rocher@isen-ouest.yncrea.fr>
2023-04-09 18:41:18 +0200

    finition de la page acceuil enseignant

--- web/acceuil-enseignant.php
+++ web/acceuil-enseignant.php
@@ -15,28 +15,29 @@
     <!-- Google icons -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <title> acceuil </title>
+    <title> acceuil enseignant</title>
 </head>
 
 <body>
-
     <!--------------------------- Navbar enseignant ---------------------------------------------------->
-
     <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
         <div class="container-fluid">
-            <img src="ISEN-blanc.png" style="width : 4.5rem; margin-right : 4px" class="img-fluid" alt="...">
+            <!--------------------------- ISEN + type de page ---------------------------------------------------->
+            <img src="ISEN-blanc.png" style="width : 4.5rem; margin-right : 8px" class="img-fluid" alt="...">
+            <a class="navbar-brand" href="#">Enseignant</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <!--------------------------- Les parties de chaque type  ---------------------------------------------------->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
+                    <!-- <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Enseignant</a>
-                    </li>
+                    </li> -->
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <a class="nav-link " href="#">
                             Mes classes
                             <span class="material-symbols-outlined" style="font-size: 1rem">
                                 table_restaurant
@@ -60,6 +61,7 @@
                         </a>
                     </li>
                 </ul>
+                <!--------------------------- Log out ---------------------------------------------------->
                 <form class="d-flex" role="search">
                     <button class="btn btn-outline-danger" type="submit">
                         Déconnexion
@@ -74,36 +76,51 @@
 
     <!--------------------------- Corps de la page ---------------------------------------------------->
     <div class="corps corps-enseignant d-flex flex-column mb-2">
-        <div class="titre" style="margin : 0">
-            <span class="material-symbols-outlined logo" style="font-size: 6rem">
+        <!--------------------------- Titre + Logo ---------------------------------------------------->
+        <div class="titre d-flex flex-column mb-2 align-items-center align-self-center">
+            <span class="material-symbols-outlined logo" style="font-size: 4rem">
                 school
             </span>
             Enseignant
         </div>
-        <div class="blocks d-flex flex-row mb-3 justify-content-evenly">
-            <div class="block align-items-center align-self-center">
-                <span class="material-symbols-outlined logo $enable-shadows" style="font-size: 8rem">
+        <!--------------------------- Block 1 ---------------------------------------------------->
+        <div class="blocks justify-content-evenly">
+            <div class="card rounded-5 text-bg-danger mb-2 align-items-center align-self-center">
+                <a href="test.php" class="stretched-link"></a>
+                <span class="material-symbols-outlined logo " style="font-size: 8rem">
                     table_restaurant
                 </span>
-                <p>
+                <p class="type">
                     Mes <br> classes
                 </p>
+
             </div>
-            <div class="block align-items-center align-self-center">
+        <!--------------------------- Block 2 ---------------------------------------------------->
+            <div class="card rounded-5 text-bg-danger mb-2 align-items-center align-self-center">
+                <a href="test.php" class="stretched-link"></a>
                 <span class="material-symbols-outlined logo" style="font-size: 8rem">
                     group
                 </span>
-                <p>
+                <p class="type">
                     Mes <br> étudiants
                 </p>
+
             </div>
-            <div class="block align-items-center align-self-center">
+        <!--------------------------- Block 3 ---------------------------------------------------->
+            <div class="card rounded-5 text-bg-danger mb-2 align-items-center align-self-center">
+                <a href="test.php" class="stretched-link"></a>
                 <span class="material-symbols-outlined logo" style="font-size: 8rem">
                     edit
                 </span>
-                <p>
-                    Edit
+                <p class="type">
+                    <br> Edit
                 </p>
+                <h3>
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark ">
+                        9
+                        <span class="visually-hidden">unread messages</span>
+                    </span>
+                </h3>
             </div>
         </div>
     </div>
