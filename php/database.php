ecb262e33b38efaee63cc761d8e43424df411542
Goustan Sermon <goustan.sermon-thuillier@isen-ouest.yncrea.fr>
2023-04-18 13:34:01 +0200

    fonction de base accès a la base de donnée

---
+++ php/database.php
@@ -0,0 +1,18 @@
+<?php
+include 'constants.php';
+
+function dbConnect(){
+    $dsn = 'pgsql:dbname='.DB_NAME.';host='.DB_SERVER.';port='.DB_PORT;
+    $user = DB_USER;
+    $password = DB_PASSWORD;
+
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+    } catch (PDOException $e) {
+        echo 'Connexion échouée : ' . $e->getMessage();
+        return false;
+    }
+    return $dbh;
+}
+
+?>
