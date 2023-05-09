3ffd018a2569f975b37da7bcea4658731bf6d3d2
Vincent Rocher <vincent.rocher@isen-ouest.yncrea.fr>
2023-05-09 11:31:19 +0200

    mise à jour

--- php/constants.php
+++ php/constants.php
@@ -1,7 +1,7 @@
 <?php
     const DB_USER = "postgres";
-    const DB_PASSWORD = "new_password";
-    const DB_NAME = "newSchool";//A definir
+    const DB_PASSWORD = "Isen44N";
+    const DB_NAME = "projet_php";//A definir
     const DB_SERVER = "127.0.0.1";
     const DB_PORT = "5432";
 ?>
