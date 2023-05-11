caa27d5aa4011805e3d14b1661d8cf650bf5d131
Goustan Sermon <goustan.sermon-thuillier@isen-ouest.yncrea.fr>
2023-05-11 20:56:13 +0200

    database V6

--- database/database.sql
+++ database/database.sql
@@ -18,17 +18,6 @@ CREATE TABLE public.personne(
 
 
 ------------------------------------------------------------
--- Table: semestre
-------------------------------------------------------------
-CREATE TABLE public.semestre(
-	id_semestre   SERIAL NOT NULL ,
-	date_debut    DATE  NOT NULL ,
-	date_fin      DATE  NOT NULL  ,
-	CONSTRAINT semestre_PK PRIMARY KEY (id_semestre)
-)WITHOUT OIDS;
-
-
-------------------------------------------------------------
 -- Table: matiere
 ------------------------------------------------------------
 CREATE TABLE public.matiere(
@@ -49,21 +38,6 @@ CREATE TABLE public.enseignant(
 
 	,CONSTRAINT enseignant_matiere_FK FOREIGN KEY (id_matiere) REFERENCES public.matiere(id_matiere)
 	,CONSTRAINT enseignant_personne0_FK FOREIGN KEY (mail) REFERENCES public.personne(mail)
-)WITHOUT OIDS;
-
-
-------------------------------------------------------------
--- Table: appreciation
-------------------------------------------------------------
-CREATE TABLE public.appreciation(
-	id_appreciation     SERIAL NOT NULL ,
-	value_apprecition   VARCHAR (250) NOT NULL ,
-	id_semestre         INT  NOT NULL ,
-	id_enseignant       INT  NOT NULL  ,
-	CONSTRAINT appreciation_PK PRIMARY KEY (id_appreciation)
-
-	,CONSTRAINT appreciation_semestre_FK FOREIGN KEY (id_semestre) REFERENCES public.semestre(id_semestre)
-	,CONSTRAINT appreciation_enseignant0_FK FOREIGN KEY (id_enseignant) REFERENCES public.enseignant(id_enseignant)
 )WITHOUT OIDS;
 
 
@@ -93,19 +67,33 @@ CREATE TABLE public.etudiant(
 
 
 ------------------------------------------------------------
+-- Table: semestre
+------------------------------------------------------------
+CREATE TABLE public.semestre(
+	id_semestre    SERIAL NOT NULL ,
+	date_debut     DATE  NOT NULL ,
+	date_fin       DATE  NOT NULL ,
+	nom_semestre   VARCHAR (50) NOT NULL ,
+	id_classe      INT  NOT NULL  ,
+	CONSTRAINT semestre_PK PRIMARY KEY (id_semestre)
+
+	,CONSTRAINT semestre_classe_FK FOREIGN KEY (id_classe) REFERENCES public.classe(id_classe)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
 -- Table: ds
 ------------------------------------------------------------
 CREATE TABLE public.ds(
 	id_evaluation   SERIAL NOT NULL ,
-	coefficient     VARCHAR (50) NOT NULL ,
-	id_classe       INT  NOT NULL ,
+	coefficient     FLOAT  NOT NULL ,
+	nom_ds          VARCHAR (50) NOT NULL ,
 	id_enseignant   INT  NOT NULL ,
 	id_semestre     INT  NOT NULL  ,
 	CONSTRAINT ds_PK PRIMARY KEY (id_evaluation)
 
-	,CONSTRAINT ds_classe_FK FOREIGN KEY (id_classe) REFERENCES public.classe(id_classe)
-	,CONSTRAINT ds_enseignant0_FK FOREIGN KEY (id_enseignant) REFERENCES public.enseignant(id_enseignant)
-	,CONSTRAINT ds_semestre1_FK FOREIGN KEY (id_semestre) REFERENCES public.semestre(id_semestre)
+	,CONSTRAINT ds_enseignant_FK FOREIGN KEY (id_enseignant) REFERENCES public.enseignant(id_enseignant)
+	,CONSTRAINT ds_semestre0_FK FOREIGN KEY (id_semestre) REFERENCES public.semestre(id_semestre)
 )WITHOUT OIDS;
 
 
@@ -123,6 +111,34 @@ CREATE TABLE public.note(
 	,CONSTRAINT note_etudiant_FK FOREIGN KEY (id_etudiant) REFERENCES public.etudiant(id_etudiant)
 	,CONSTRAINT note_ds0_FK FOREIGN KEY (id_evaluation) REFERENCES public.ds(id_evaluation)
 	,CONSTRAINT note_enseignant1_FK FOREIGN KEY (id_enseignant) REFERENCES public.enseignant(id_enseignant)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: appreciation
+------------------------------------------------------------
+CREATE TABLE public.appreciation(
+	id_appreciation     SERIAL NOT NULL ,
+	value_apprecition   VARCHAR (250) NOT NULL ,
+	id_semestre         INT  NOT NULL ,
+	id_enseignant       INT  NOT NULL  ,
+	CONSTRAINT appreciation_PK PRIMARY KEY (id_appreciation)
+
+	,CONSTRAINT appreciation_semestre_FK FOREIGN KEY (id_semestre) REFERENCES public.semestre(id_semestre)
+	,CONSTRAINT appreciation_enseignant0_FK FOREIGN KEY (id_enseignant) REFERENCES public.enseignant(id_enseignant)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: admin
+------------------------------------------------------------
+CREATE TABLE public.admin(
+	mail           VARCHAR (50) NOT NULL ,
+	nom            VARCHAR (50) NOT NULL ,
+	prenom         VARCHAR (50) NOT NULL ,
+	mot_de_passe   VARCHAR (200) NOT NULL ,
+	telephone      VARCHAR (50) NOT NULL  ,
+	CONSTRAINT admin_PK PRIMARY KEY (mail)
 )WITHOUT OIDS;
 
 
