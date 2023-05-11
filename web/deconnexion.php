683a2df8ccd6a3cf266cae093987a9cd8204d544
Vincent Rocher <vincent.rocher@isen-ouest.yncrea.fr>
2023-05-11 21:09:34 +0200

    résoudre logout

--- web/deconnexion.php
+++ web/deconnexion.php
@@ -2,5 +2,5 @@
 session_start();
 $_SESSION = array();
 session_destroy();
-header("Location: http://localhost/php/CIR2_WebProject-1/web/identification.php");
+header("Location: /web/identification.php");
 ?>
