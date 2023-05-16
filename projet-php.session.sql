0e313195c3df34bab38a0e0ef70383bfd91bca8c
Goustan Sermon <goustan.sermon-thuillier@isen-ouest.yncrea.fr>
2023-05-16 12:13:46 +0200

    maj data.sql

--- projet-php.session.sql
+++ projet-php.session.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-SELECT appreciation.* FROM appreciation JOIN consulter ON appreciation.id_appreciation = consulter.id_appreciation WHERE appreciation.id_matiere = 1 AND appreciation.id_semestre = 7 AND consulter.id_etudiant = 1;
-=======
-SELECT * FROM semestre WHERE id_semestre IN (SELECT id_semestre FROM ds WHERE id_evaluation IN (SELECT id_evaluation FROM note WHERE id_etudiant = 1)) AND date_debut <= '2023-05-15' AND date_fin >= '2023-05-15';
->>>>>>> f146a90251b4e74d4b120f111abe2dbad4eeed1b
+SELECT DISTINCT classe.* FROM classe JOIN ds On classe.id_classe = ds.id_classe WHERE ds.id_enseignant = 4 AND ds.id_semestre = 5;
